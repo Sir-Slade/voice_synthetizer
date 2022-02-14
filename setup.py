3c5f8f3cb263c681f3ce975481f6e17178651c1b
Jeshua <shark12_venom@hotmail.com>
2022-02-14 22:53:31 +0000

    Created the necessary setup.py to install with conda

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "synth_voice",
+    version = "1.0.0",
+    author = "Jeshua Perzabal",
+    description = ("A library that enables to synthesize tts from existing Tacotron2 models."),
+    url = "https://github.com/Sir-Slade/voice_synthetizer.git",
+    packages=['synth_voice'],
+    install_requires=["sounddevice"],
+)

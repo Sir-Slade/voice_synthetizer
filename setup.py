3f11f915de44c2f3ab3884ef069933c361fe5ca9
Jeshua <shark12_venom@hotmail.com>
2022-02-15 03:28:51 +0000

    Added dependencie for sounddevice again since Im calling pip without deps on the meta.yaml file

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version = "1.0.0",
     author = "Jeshua Perzabal",
     description = "A library that enables to synthesize tts from existing Tacotron2 models.",
-    url = "https://github.com/Sir-Slade/voice_synthetizer.git",
+    url = "https://github.com/Sir-Slade/voice_synthetizer",
     packages=['synth_voice'],
+    install_requires=["sounddevice"],
 )

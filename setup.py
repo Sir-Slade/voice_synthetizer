2bbb0ceba214e51103dce4da318e278d033d34c8
Jeshua <shark12_venom@hotmail.com>
2022-02-15 00:17:20 +0000

    Another modification

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = "synth-voice",
     version = "1.0.0",
     author = "Jeshua Perzabal",
-    description = ("A library that enables to synthesize tts from existing Tacotron2 models."),
+    description = "A library that enables to synthesize tts from existing Tacotron2 models.",
     url = "https://github.com/Sir-Slade/voice_synthetizer.git",
     packages=['synth_voice'],
 )

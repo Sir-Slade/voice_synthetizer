dfd88927754897b722586c04c5e88384a61c35e1
Jeshua <shark12_venom@hotmail.com>
2022-02-16 22:29:23 +0000

    Replaced setup.py and meta.yaml with versions from the cookiecutter that produced a package correctly

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup
 
 setup(
     name = "synth_voice",
-    version = "1.0.0",
+    version = "0.0.1",
     author = "Jeshua Perzabal",
     description = "A library that enables to synthesize tts from existing Tacotron2 models.",
     url = "https://github.com/Sir-Slade/voice_synthetizer",
     packages=['synth_voice'],
     install_requires=["sounddevice"],
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+    ]
 )
